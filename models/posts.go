90b0736ce06654731cc146ace584c0a41abd77a3
Said_Maratov <maratov.said@icloud.com>
2020-08-25 15:42:46 +0600

    lol

--- models/posts.go
+++ models/posts.go
@@ -4,81 +4,82 @@ import (
 	"errors"
 	"fmt"
 )
+
 type Post struct {
 	Id          string
 	Description string
 	PostDate    string
-	UserId      string
-	Category 	string
-	Theme 		string
+	User        User
+	Category    Category
+	Title       string
 }
 
-func AllPosts() ([]Post,error) {
-	rows,err := Db.Query("SELECT * FROM Post")
+func AllPosts() ([]Post, error) {
+	rows, err := Db.Query("SELECT * FROM Post")
 	var posts []Post
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 	for rows.Next() {
 		post := Post{}
-		err := rows.Scan(&post.Id,&post.Description,&post.PostDate,&post.UserId,&post.Category,&post.Theme)
+		err := rows.Scan(&post.Id, &post.Description, &post.PostDate, &post.User.Id, &post.Category, &post.Title)
 		if err != nil {
 			return nil, err
 		}
-		posts = append(posts,post)
+		posts = append(posts, post)
 	}
-	return posts,nil
+	return posts, nil
 }
 
-func AddPost(post Post,sql SQLDB) error {
-	_,err := sql.Exec("INSERT INTO POST (Id,Description,Post_date,UserId,Category,Theme) values ($1,$2,$3,$4,$5,$6)",post.Id,post.Description,post.PostDate,post.UserId,post.Category,post.Theme)
-	if err != nil { 
+func AddPost(post Post, sql SQLDB) error {
+	_, err := sql.Exec("INSERT INTO POST (Id,Description,Post_date,UserId,CategoryId,Title) values ($1,$2,$3,$4,$5,$6)", post.Id, post.Description, post.PostDate, post.User.Id, post.Category.Id, post.Title)
+	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func PostById(id string) (Post,error) {
+func PostById(id string) (Post, error) {
 	post := Post{}
 	query := fmt.Sprintf("SELECT * FROM Post WHERE Id LIKE '%s'", id)
-	rows,err := Db.Query(query)
+	rows, err := Db.Query(query)
 	if err != nil {
-		return Post{},err
+		return Post{}, err
 	}
 
 	for rows.Next() {
-		err := rows.Scan(&post.Id,&post.Description,&post.PostDate,&post.UserId,&post.Category,&post.Theme)
+		err := rows.Scan(&post.Id, &post.Description, &post.PostDate, &post.User.Id, &post.Category, &post.Title)
 		if err != nil {
-			return Post{},err
+			return Post{}, err
 		}
 	}
 
-	return post,nil
+	return post, nil
 }
 
-func SortedPosts(sortBy string,user User) ([]Post,error) {
+func SortedPosts(sortBy string, user User) ([]Post, error) {
 	var query string
 	var posts []Post
 
 	if sortBy == "created" {
 		query = fmt.Sprintf("SELECT * FROM POST ORDER BY CASE userid WHEN '%s' THEN 1 ELSE 2 END;", user.Username)
-	}else {
-		return posts,errors.New("no such parameter to sort")
+	} else {
+		return posts, errors.New("no such parameter to sort")
 	}
 
-	rows,err := Db.Query(query)
+	rows, err := Db.Query(query)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	for rows.Next() {
 		post := Post{}
-		err := rows.Scan(&post.Id,&post.Description,&post.PostDate,&post.UserId,&post.Category,&post.Theme)
+		err := rows.Scan(&post.Id, &post.Description, &post.PostDate, &post.User.Id, &post.Category, &post.Title)
 		if err != nil {
-			return nil,err
+			return nil, err
 		}
-		posts = append(posts,post)
+		posts = append(posts, post)
 	}
 
-	return posts,nil
+	return posts, nil
 }
